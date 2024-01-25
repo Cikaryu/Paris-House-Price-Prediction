74874fdb8e903e7ad034c873e4c0f1335a0ade70
dekjul <dekjul666@gmail.com>
2024-01-25 15:06:11 +0800

    Update featureselection.py

--- featureselection.py
+++ featureselection.py
@@ -10,7 +10,7 @@ def load_data():
 
 def feature():
     st.title("Feature Selection")
-    st.write("Selamat datang di bagian program aplikasi prediksi harga rumah di Paris.")
+    st.write("Welcome to the feature selection section of our Paris House Price Prediction application! In this part, we are preparing to build a model that can predict house prices based on various features. Let me walk you through what's happening:.")
     
     # Memanggil fungsi load_data dengan caching
     dataset = load_data()
@@ -27,6 +27,7 @@ def feature():
 
 # Display feature importances in a table
     st.subheader("Feature Importances:")
+    st.write("The application analyzes the importance of each feature in predicting house prices. The 'Feature Importances' table displays the contribution of each feature to the predictive power of the model. This information helps us understand which features play a significant role in determining house prices.")
     feature_importance_df = pd.DataFrame({
         'Feature': X_train.columns,
         'Importance': feature_importances
@@ -41,6 +42,7 @@ def feature():
 
 # Display feature correlations in a table
     st.subheader("Feature Correlations with Price:")
+    st.write("We explore the correlation between different features and the house prices. The 'Feature Correlations with Price' table shows how strongly each feature correlates with the price. High correlations indicate strong relationships.")
     feature_correlation_df = pd.DataFrame({
         'Feature': feature_correlation.index,
         'Correlation': feature_correlation.values
@@ -49,6 +51,7 @@ def feature():
 
     # Scatter plot
     st.subheader("Scatter plot between price and squareMeters:")
+    st.write("To provide a visual representation, we display a scatter plot between house prices and the square footage of houses. This plot gives an intuitive sense of how the price varies with the size of the property.")
     fig, ax = plt.subplots()
     sns.scatterplot(data=dataset, x="price", y="squareMeters", ax=ax)
     st.pyplot(fig)  # Display the scatter plot in Streamlit
