3be1921cca14db336a616c459e245ee00e785241
dekjul <dekjul666@gmail.com>
2024-01-25 15:58:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dataloader import load_data
 with st.sidebar:
     selected = option_menu(
         menu_title="Paris House Prediction",
-        options=["Introduction","Data Exploration","Feature Selection"],
+        options=["Introduction","Predict","Data Exploration","Feature Selection"],
         menu_icon="cast",
         default_index = 0,  
     )
@@ -15,17 +15,67 @@ if selected == "Introduction":
 
     image_url = "https://assets.kompasiana.com/items/album/2015/08/04/www-telegraph-co-uk-55c0663792937374048b4567.jpg?v=600&t=o?t=o&v=740&x=416"
     st.image(image_url, use_column_width=True)
-     
+
+    # Introduction
     st.markdown(
     """
     <div style="text-align: justify;">
-        In this machine learning project to predict house prices in Paris using regression methods, we use the Linear Regression algorithm to identify the relationship between various property features (such as size, location, and number of rooms, age of the house, etc.) and house prices. 
-        By utilizing a dataset that includes historical house data, the model learns to make price predictions based on certain characteristics. Once trained, the model can accept new inputs about the features of the house and produce an output in the form of a price estimate. 
-        This output can provide valuable information to potential buyers, sellers, or homeowners to make more informed decisions in the context of the Paris real estate market.
+        Welcome to our Paris House Price Prediction application! Our goal is to provide you with a powerful tool to 
+        estimate housing prices in the beautiful city of Paris. Whether you are a potential homebuyer, investor, or 
+        simply curious about the real estate market in Paris, our model is designed to assist you in making informed decisions.
+    </div>
+    """),
+
+    # About App
+    st.markdown("<h3 class='centered'>About the Application</h3>", unsafe_allow_html=True)
+    st.markdown(
+    """
+    <div style="text-align: justify;">
+        Our application utilizes advanced machine learning algorithms to analyze various factors influencing house prices 
+        in Paris. By inputting specific features such as the number of rooms, square footage, and location, our model 
+        generates accurate predictions to help you understand the market dynamics.
     </div>
     """,
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
+    
+    # How To Use
+    st.markdown("<h3 class='centered'>How to Use the Prediction Feature</h3>", unsafe_allow_html=True)
+    st.markdown(
+    """
+    <div style="text-align: justify;">
+        Get personalized predictions for house prices by filling out the simple input form. Explore how changing 
+        parameters such as the size of the property or its location can impact the estimated price. Our application 
+        aims to empower you with the information needed to make confident decisions in the housing market.
+    </div>
+    """,
+    unsafe_allow_html=True)
+    
+    # Stay Informed
+    st.markdown("<h3 class='centered'>Stay Informed</h3>", unsafe_allow_html=True)
+    st.markdown(
+    """
+    <div style="text-align: justify;">
+        Stay up-to-date with the latest trends and developments in the Paris real estate scene. Our application 
+        is not just a prediction tool but a comprehensive resource to keep you informed about the ever-evolving 
+        landscape of property values in the city of lights.
+    </div>
+    """,
+    unsafe_allow_html=True)
+
+    # Start Exploring
+    st.markdown("<h2 class='centered'>Start Exploring</h2>", unsafe_allow_html=True)
+    st.markdown(
+    """
+    <div style="text-align: justify;">
+        Ready to dive in? Begin your journey into the world of Parisian real estate by navigating through the different 
+        sections of our application. Whether you're a first-time homebuyer or a seasoned investor, our goal is to make 
+        your experience seamless and informative.
+    </div>
+    """,
+    unsafe_allow_html=True)
+    st.markdown("<h5 class='centered'>Thank you for choosing Paris House Price Prediction. Let's explore the future of real estate together!</h5>", unsafe_allow_html=True)
+    
+    
 
 elif selected == "Data Exploration":
     dataset = load_data()
